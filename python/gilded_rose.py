d07d4ada014eedee07200939d383b791e5441807
bernatosorioigomez <bernatosorioigomez@gmail.com>
2022-11-25 13:28:47 +0100

    update_quality fixed

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -11,6 +11,9 @@ class GildedRose(object):
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
                         item.quality = item.quality - 1
+                        if "conjured" in item.name.lower():
+                            if item.quality > 0:
+                                item.quality = item.quality - 1
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -6,10 +6,34 @@ from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
-        items = [Item("foo", 0, 0)]
+        items = [Item("Conjured foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEqual("Conjured foo", items[0].name)
+        self.assertEqual(0, items[0].quality)
+        self.assertEqual(-1, items[0].sell_in)
+
+        items = [Item("Conjured foo", 5, 5)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+
+        self.assertEqual("Conjured foo", items[0].name)
+        self.assertEqual(3, items[0].quality)
+        self.assertEqual(4, items[0].sell_in)
+
+        gilded_rose.update_quality()
+
+        self.assertEqual("Conjured foo", items[0].name)
+        self.assertEqual(1, items[0].quality)
+        self.assertEqual(3, items[0].sell_in)
+
+        gilded_rose.update_quality()
+
+        self.assertEqual("Conjured foo", items[0].name)
+        self.assertEqual(0, items[0].quality)
+        self.assertEqual(2, items[0].sell_in)
+
+
 
         
 if __name__ == '__main__':
